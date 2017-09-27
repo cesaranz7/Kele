c6478ced5bee6508328fe03a693c013a0f03d502
cesaranz7 <cesaranz@hotmail.com>
2017-09-26 23:16:13 -0400

    retrieves current user data

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,20 @@
 require 'httparty'
 
+
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
+    #Retrieves and stores authorization token when passing valid credentials
     response = self.class.post('/sessions', body: { email: "#{email}", password: "#{password}" } )
     @auth_token = response["auth_token"]
     raise "ERROR: Wrong email or password." if @auth_token.nil?
+  end
+
+  def get_me
+    ### Retrieve the current user from the Bloc API and converting into a ruby hash
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    body = JSON.parse(response.body)
   end
 end
