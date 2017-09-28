343796015c71fad55155a1854639997a7781835f
cesaranz7 <cesaranz@hotmail.com>
2017-09-27 21:45:24 -0400

    adds ability to retrieve mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -17,4 +17,11 @@ class Kele
     response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
     body = JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get('/mentors/' + mentor_id.to_s + '/student_availability', headers: {"authorization" => @auth_token })
+    body = JSON.parse(response.body)
+    mentor_availability = []
+    body.each { |time_slot| mentor_availability << time_slot if time_slot['booked'] == nil }
+  end
 end
